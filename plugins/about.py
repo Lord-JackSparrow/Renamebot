6bac1797ec3742e95188b266a64eaef131f1551e
J͙a͙c͙k͙ S͙p͙a͙r͙r͙o͙w͙ <138965244+Lord-JackSparrow@users.noreply.github.com>
2023-11-15 15:39:06 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -12,4 +12,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Support Us:- <a href='https://t.me/pathan_botz'> Join Us </a>\nCreater :- <a href='https://t.me/Flipkartlootzs'>🔥 Pathan Botz🔥</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- Heruko\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you For using our bot support us*",quote=True)
+	await message.reply_text(f"Support Us:- <a href='https://t.me/elitecraft_studios> Join Us </a>\nCreater :- <a href='https://t.me/god_luffy_ati'>IT'S LUFFY</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- Heruko\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you For using our bot support us*",quote=True)
